1f6bfb03cb9f1fa33653f58983f3fac0f9986e4e
Kevin Angstadt <angstadt@virginia.edu>
2020-03-07 20:52:14 -0500

    More updates to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# os-level dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential git
+sudo apt-get install -y ant build-essential default-jdk-headless git \
+                        python-minimal python-pip
 
+# download sub-repos
 git submodule update --init
 
+# install python packages
+sudo pip2 install --upgrade pip
 sudo pip2 install -r requirements.txt
+
+# build cpachecker
+cd cpachecker
+ant
+cd ..
